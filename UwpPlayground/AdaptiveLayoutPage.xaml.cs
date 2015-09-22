21dd86d19969bb3ac35bc476aabe8c5d54e6402e
Andreas Hammar <andreas.hammar@jayway.com>
2015-09-22 13:47:35 +0200

    long list

--- UwpPlayground/AdaptiveLayoutPage.xaml.cs
+++ UwpPlayground/AdaptiveLayoutPage.xaml.cs
@@ -25,8 +25,23 @@ namespace UwpPlayground
             this.InitializeComponent();
 
             _colors = typeof (Colors).GetProperties().ToList();
-            ColorVmis = _colors.Select(x => new {x.Name, Brush = new SolidColorBrush((Color)x.GetValue(x))}).ToList();
-            
+            var systemColors = _colors.Select(x => new { x.Name, Brush = new SolidColorBrush((Color)x.GetValue(x)) }).ToList();
+
+            var allColors = new List<object>();
+            for (byte i = 0; i <= 254; i = (byte)Math.Min(255, i + 10))
+            {
+                for (byte j = 0; j <= 254; j = (byte)Math.Min(255, j + 10))
+                {
+                    for (byte k = 0; k <= 254; k = (byte)Math.Min(255, k + 10))
+                    {
+                        var color = new {Name=$"{i},{j},{k}", Brush=new SolidColorBrush(Color.FromArgb(255, i, j, k))};
+                        allColors.Add(color);
+                    }
+                }
+            }
+
+            ColorVmis = allColors;
+
             DataContext = this;
         }
 
