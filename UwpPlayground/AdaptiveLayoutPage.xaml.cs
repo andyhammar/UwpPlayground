b454d7df6ec312b76678e66f252687fe7d326154
Andreas Hammar <andreas.hammar@jayway.com>
2015-09-22 12:44:23 +0200

    deferloadstrategy

--- UwpPlayground/AdaptiveLayoutPage.xaml.cs
+++ UwpPlayground/AdaptiveLayoutPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using System.Diagnostics;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -12,6 +15,23 @@ namespace UwpPlayground
         public AdaptiveLayoutPage()
         {
             this.InitializeComponent();
+        }
+
+        private void wideOnlyButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+
+        private void BlueBorder_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            try
+            {
+                Debug.WriteLine(wideOnlyButton.Content);
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception);
+            }
         }
     }
 }
