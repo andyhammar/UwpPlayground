34dcb02aaaa66e6b428fd0dec7378daefac4f159
Andreas Hammar <andreas.hammar@jayway.com>
2015-09-23 09:44:08 +0200

    x.bind

--- UwpPlayground/AdaptiveLayoutPage.xaml.cs
+++ UwpPlayground/AdaptiveLayoutPage.xaml.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using Windows.UI;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using UwpPlayground.Annotations;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -15,17 +19,26 @@ namespace UwpPlayground
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class AdaptiveLayoutPage : Page
+    public sealed partial class AdaptiveLayoutPage : Page, INotifyPropertyChanged
     {
-        private readonly List<PropertyInfo> _colors;
+        private string _redText;
         public object ColorVmis { get; set; }
+
+        public string RedText
+        {
+            get { return _redText; }
+            set { _redText = value; OnPropertyChanged();}
+        }
 
         public AdaptiveLayoutPage()
         {
             this.InitializeComponent();
 
-            _colors = typeof (Colors).GetProperties().ToList();
-            var systemColors = _colors.Select(x => new { x.Name, Brush = new SolidColorBrush((Color)x.GetValue(x)) }).ToList();
+            RedText = "first red text";
+
+            
+            var colors = typeof (Colors).GetProperties().ToList();
+            var systemColors = colors.Select(x => new { x.Name, Brush = new SolidColorBrush((Color)x.GetValue(x)) }).ToList();
 
             var allColors = new List<object>();
             for (byte i = 0; i <= 254; i = (byte)Math.Min(255, i + 10))
@@ -48,6 +61,8 @@ namespace UwpPlayground
 
         private void BlueBorder_OnTapped(object sender, TappedRoutedEventArgs e)
         {
+            RedText = "blueborder was tapped";
+
             try
             {
                 Debug.WriteLine(wideOnlyList.ActualHeight);
@@ -60,7 +75,47 @@ namespace UwpPlayground
 
         private void RedBorder_OnTapped(object sender, TappedRoutedEventArgs e)
         {
+            RedText = "redborder was tapped";
+        }
+
+        private void RelativePanel_Tapped(object sender, TappedRoutedEventArgs e)
+        {
             
+            //RedText = "relpanel was tapped";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Button1_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+            UpdateRedText();
+        }
+
+        private void Button2_OnClick(object sender, RoutedEventArgs e)
+        {
+            UpdateRedText();
+        }
+
+        private void UpdateRedText([CallerMemberName]string sender = null)
+        {
+            RedText = sender;
+        }
+
+        private void SuspendingBindingsButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Bindings.StopTracking();
+        }
+
+        private void ResumeBindingsButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Bindings.Update();
         }
     }
 }
