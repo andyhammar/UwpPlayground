3749abf7d5695cd0365680dc5590566aeed29daa
Andreas Hammar <andreas.hammar@jayway.com>
2015-09-23 14:13:34 +0200

    devicefamily

---
+++ UwpPlayground/DevicePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UwpPlayground
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DevicePage : Page
+    {
+        public DevicePage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var canGoBack = ((Frame)Window.Current.Content).CanGoBack;
+            var appViewBackButtonVisibility = canGoBack ? AppViewBackButtonVisibility.Visible : AppViewBackButtonVisibility.Collapsed;
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                appViewBackButtonVisibility;
+
+        }
+    }
+}
