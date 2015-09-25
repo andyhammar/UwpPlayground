2ed86b9d3ded6cd4770e68fa385a02ecb2dd7722
Andreas Hammar <andreas.hammar@jayway.com>
2015-09-25 09:53:54 +0200

    2nd tile

--- UwpPlayground/TilePage.xaml.cs
+++ UwpPlayground/TilePage.xaml.cs
@@ -1,5 +1,12 @@
 ﻿// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
+using System;
+using Windows.Foundation;
+using Windows.UI.Notifications;
+using Windows.UI.StartScreen;
+using Windows.UI.Xaml;
+using NotificationsExtensions.Tiles;
+
 namespace UwpPlayground
 {
     /// <summary>
@@ -10,6 +17,60 @@ namespace UwpPlayground
         public TilePage()
         {
             this.InitializeComponent();
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var secondaryTile = new SecondaryTile(
+                "tilePage",
+                "shortname",
+                "displayname",
+                new Uri("ms-appx:///Assets/StoreLogo.png", UriKind.Absolute),
+                TileSize.Default);
+            var success = await secondaryTile.RequestCreateAsync();
+            if (success)
+            {
+                var tileBindingContentAdaptive = new TileBindingContentAdaptive
+                {
+                    Children =
+                    {
+                        new TileText
+                        {
+                            Text = "uwp tiles",
+                            Style = TileTextStyle.Header
+                        },
+                        new TileText
+                        {
+                            Text = "secondary",
+                            Style = TileTextStyle.Body
+                        }
+                    }
+                };
+                var tileBinding = new TileBinding
+                {
+                    Branding = TileBranding.Name,
+                    Content = tileBindingContentAdaptive,
+                    DisplayName = "my tile"
+                };
+
+                var tileContent = new TileContent
+                {
+                    Visual = new TileVisual
+                    {
+                        TileMedium = tileBinding,
+                        TileWide = tileBinding,
+                        TileLarge = tileBinding
+                    }
+                };
+
+                var xmlDoc = tileContent.GetXml();
+
+
+                TileNotification tileNotification = new TileNotification(xmlDoc);
+                var tileUpdaterForSecondaryTile = TileUpdateManager.CreateTileUpdaterForSecondaryTile("tilePage");
+                tileUpdaterForSecondaryTile.Update(tileNotification);   
+            }
+
         }
     }
 }
