e454daa9ec7aa358215bb2646dd249f855308896
Andreas Hammar <andreas.hammar@jayway.com>
2015-10-07 22:47:54 +0200

    added battery page

---
+++ UwpPlayground/BatteryPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+using Windows.Devices.Power;
+using Windows.UI.Xaml.Navigation;
+
+namespace UwpPlayground
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BatteryPage
+    {
+        public BatteryPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var battery = Windows.Devices.Power.Battery.AggregateBattery;
+            var report = battery.GetReport();
+            _batteryTextBlock.Text = $"{report.Status},{report.ChargeRateInMilliwatts},{report.DesignCapacityInMilliwattHours},{report.FullChargeCapacityInMilliwattHours},{report.RemainingCapacityInMilliwattHours}";
+        }
+    }
+}
